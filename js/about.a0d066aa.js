c3376f22e1077e42dacced824f78cc4ec5c5cfdc
JamPeng <pengzhe.info@gmail.com>
2022-05-20 21:05:03 +0800

    dist init

---
+++ js/about.a0d066aa.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkvuecli_test"]=self["webpackChunkvuecli_test"]||[]).push([[443],{7381:function(t,e,n){n.r(e),n.d(e,{default:function(){return f}});var u=n(3396);const s={class:"about"},a=(0,u._)("h1",null,"This is an about page",-1),c=[a];function r(t,e){return(0,u.wg)(),(0,u.iD)("div",s,c)}var i=n(89);const l={},o=(0,i.Z)(l,[["render",r]]);var f=o}}]);
+//# sourceMappingURL=about.a0d066aa.js.map
